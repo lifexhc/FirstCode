d772fb9c3bfb95d3cda9babedb3559a4123864d4
xuhongchuan <life.xhc@gmail.com>
2016-05-17 14:20:34 +0800

    ADD:chapters7

---
+++ chapters7/ProviderTest/app/src/main/java/com/xuhongchuan/providertest/MainActivity.java
@@ -0,0 +1,80 @@
+package com.xuhongchuan.providertest;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String newId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button addData = (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("content://com.xuhongchuan.databasetest.provider/book");
+                ContentValues values = new ContentValues();
+                values.put("name", "我的互联网方法论");
+                values.put("author", "周鸿祎");
+                values.put("pages", "1000");
+                values.put("price", "19.9");
+                Uri newUri = getContentResolver().insert(uri, values);
+                newId = newUri.getPathSegments().get(1);
+            }
+        });
+
+        Button queryData = (Button) findViewById(R.id.query_data);
+        queryData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("content://com.xuhongchuan.databasetest.provider/book");
+                Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+                if (cursor != null) {
+                    while (cursor.moveToNext()) {
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price = cursor.getDouble(cursor.getColumnIndex("price"));
+                        Log.d("MainActivity", "book name is " + name);
+                        Log.d("MainActivity", "book author is " + author);
+                        Log.d("MainActivity", "book pages is " + pages);
+                        Log.d("MainActivity", "book price is " + price);
+                    }
+                    cursor.close();
+                }
+            }
+        });
+
+        Button updateData = (Button) findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("content://com.xuhongchuan.databasetest.provider/book/" + newId);
+                ContentValues values = new ContentValues();
+                values.put("name", "我的互联网方法论");
+                values.put("pages", "1024");
+                values.put("price", "29.9");
+                getContentResolver().update(uri, values, null, null);
+            }
+        });
+
+        Button deleteData = (Button) findViewById(R.id.delete_data);
+        deleteData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("content://com.xuhongchuan.databasetest.provider/book/" + newId);
+                getContentResolver().delete(uri, null, null);
+            }
+        });
+    }
+}
